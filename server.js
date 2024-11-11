c029d501b6d2e25bc88f615642003cb8c22cc88d
Rudrakumar Patel <rudrapatel2992003@gmail.com>
2024-11-11 22:38:38 +0530

    update

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-const _dirname = path.resolve();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -25,22 +24,23 @@ mongoose.connect(process.env.MONGO_DB)
   .catch(err => console.error('Failed to connect to MongoDB', err));
 
 
-app.use(express.static(path.resolve(_dirname, "auth-frontend", "dist")));
-app.use(express.json());
-app.use(express.static('public'))
-app.use('/images',express.static("images"))
+// app.use(express.static(path.resolve(_dirname, "auth-frontend", "dist")));
+// app.use(express.json());
+// app.use(express.static('public'))
+// app.use('/images',express.static("images"))
 
-app.get("*",(req, res) => {
-    res.sendFile(path.resolve(_dirname,"auth-frontend","dist","index.html"));
-})
+// app.get("*",(req, res) => {
+//     res.sendFile(path.resolve(_dirname,"auth-frontend","dist","index.html"));
+// })
 
 // Use the auth routes
 app.use('/api/auth', authRoutes);
 
-// app.get("/",(req, res) =>{
-//   app.use(express.static(path.resolve(__dirname,"auth-frontend","dist")));
-//   res.sendFile(path.resolve(__dirname,"auth-frontend","dist","index.html"));  // send back index.html for all other routes
-// })
+app.get("/",(req,res)=>
+  {
+      app.use(express.static(path.resolve(__dirname,"frontend","dist")));
+      res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+  })
 
 // Start the server
 app.listen(PORT, () => {
